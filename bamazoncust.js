f0518f8ad034e2d71246013551ca962a2f866736
Raj Sodhi <rajsodhiesq@gmail.com>
2019-10-03 09:40:08 -0700

    its working

--- bamazoncust.js
+++ bamazoncust.js
@@ -11,7 +11,15 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
     if (err) throw err;
-    console.log("Connected!");
+    console.log("Welcome to Beach City Traders!");
+    // console.log("   _____ ____ _____");
+    // console.log("  /    /      \    \ ");
+    // console.log("/____ /_________\____\ ");
+    // console.log("\    \          /    / ");
+    // console.log("   \  \        /  / ");
+    // console.log("      \ \    / / ");
+    // console.log("        \ \/ / ");
+    // console.log("          \/");
 
     openStore();
   });
@@ -26,8 +34,63 @@ var openStore = function() {
             + res[i].stock_quantity);
             console.log("------------------------------------------------");
             }
+            prompt(res);
         }
     )}
+
+var prompt = function(res) {
+    inquirer
+    .prompt({
+        type: "input",
+        name: "purchase",
+        message: "Hey did you see something you like? Please tell me the item ID! Type bye to leave, if you really want :("
+    })
+    
+    .then(function(answer) {
+        console.log(answer);
+       // return
+        var match = false;
+        var quit = answer.purchase;
+        if (quit == "bye"){
+            process.exit();
+        }
+        for (var i=0; i <res.length; i++) {
+            if (answer.purchase == res[i].item_id) {
+                match = true;
+                var id = i;
+                inquirer
+                .prompt({
+                    type: "input",
+                    name: "amount",
+                    message: "That's great! How many would you like to buy?",
+                    validate: function(number) {
+                        if (isNaN(number) == false) {
+                            return true;
+                        } else {
+                                return false;
+                            }
+                        }
+                    })
+                    .then(function(answer) {
+                        if((res[id].stock_quantity - answer.amount) > 0) {
+                            connection.query("UPDATE products SET stock_quantity ='" + (res[id].stock_quantity - answer.amount) +"' WHERE item_id = '"+id+"'",
+                            function(err, res2) {
+                                console.log("You got it, thanks!");
+                                openStore();
+                            })
+                        } else {
+                            console.log("I'm so sorry that doesn't work!");
+                            prompt(res);
+                        }
+                    })
+                }           
+            }
+            if(i == res.length && match==false) {
+                console.log("I'm so sorry that doesn't work!");
+                prompt(res);
+            }
+    })
+}
 
 
 
